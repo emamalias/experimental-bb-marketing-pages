6c52bc71e84aa98db9142a94c5be68fc4fb20e43
Elias <mamalias23@gmail.com>
2024-08-15 03:53:19 +0800

    mdx

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -4,11 +4,12 @@ import tailwind from '@astrojs/tailwind';
 import cloudflare from "@astrojs/cloudflare";
 import react from '@astrojs/react'
 import markdoc from '@astrojs/markdoc'
+import mdx from '@astrojs/mdx';
 import keystatic from '@keystatic/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), markdoc(), keystatic()],
+  integrations: [tailwind(), react(), markdoc(), mdx(), keystatic()],
   output: "server",
   adapter: cloudflare({
     imageService: 'passthrough',
